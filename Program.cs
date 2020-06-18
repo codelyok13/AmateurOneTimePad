dfa50f8660c4caeaad954d5b63dc213160e2a0ff
Kendricks Camille <codelyok13@live.com>
2020-06-17 22:29:38 -0400

    Removed unnecessary variables. For example, data[1] was the randomly generated value that was stored in another variable called randomBytes. This is completely unnecessary.
    All the arrays are the exact same size so there is no reason to keep
    re-allocating the array when it just be done in memory. For example, if
    I store the encrypted file in data[0], I can just update data[0] with
    the random value from data[1] then I can convert data[0] to its
    encrypted form.

--- Program.cs
+++ Program.cs
@@ -9,9 +9,7 @@ namespace OneTimeLock
     class Program
     {
         static Random r; //Generates random values
-        static byte[][] data; //stores encrypted data and key
-        static byte[] randomBytes; //stores the random bytes made
-        static byte[] file; //stores the file to be encrypted
+        static byte[][] data = new byte[2][]; //stores encrypted data and key
 
         /*
             The largest size allowable for an array is some value below 2GBs. 
@@ -62,96 +60,42 @@ namespace OneTimeLock
             Console.WriteLine(message);
         }
 
-        static void Encrypt(string path)
-        {
-            file = File.ReadAllBytes(path);
-            r = new Random();
-
-            string pathName = Path.GetFileName(path);
-
-            using(FileStream fs1 = new FileStream(pathName+".onetp",FileMode.Create,FileAccess.ReadWrite))
-            {
-                using(FileStream fs2 = new FileStream(pathName+".key",FileMode.Create,FileAccess.ReadWrite))
-                {
-                    randomBytes = new byte[file.Length]; 
-
-                    int i = 0;
-                    r.NextBytes(randomBytes);
-                    foreach(byte b in randomBytes)
-                    {
-                        fs2.WriteByte(b);
-                        byte encrypt = (byte)(file[i]+b);
-                        unchecked{ fs1.WriteByte(encrypt); }
-                        i++;
-                    }
-                }
-            }
-        }
-        
-        static void Decrypt(string encryptedText, string key )
-        {
-            string nameOfFile = Path.GetFileNameWithoutExtension(key);
-            using(FileStream fs3 = new FileStream(nameOfFile,FileMode.Create,FileAccess.ReadWrite))
-            {
-                byte[] byteFS1 = File.ReadAllBytes(encryptedText);
-                byte[] byteFS2 = File.ReadAllBytes(key);
-                Console.WriteLine(byteFS2.Length +" "+ byteFS1.Length);
-                if(byteFS1.Length != byteFS2.Length){throw new Exception("The size of the key and encrypted text are diffrent.");}
-
-                for(int i = 0; i < byteFS1.Length;i++)
-                {
-                    fs3.WriteByte((byte)(byteFS1[i]-byteFS2[i]));
-                }
-                
-                File.Delete(encryptedText);
-                File.Delete(key);
-            }
-        }
-
         static void ParallelEncrypt(string path)
         {
-            file = File.ReadAllBytes(path);
-            
-            randomBytes = new byte[file.Length]; 
+            data[0] = File.ReadAllBytes(path);
+            data[1] = new byte[data[0].Length];
             r = new Random();
-            r.NextBytes(randomBytes);
+            r.NextBytes(data[1]);
 
             string pathName = Path.GetFileName(path);
             
-            
-            //0 is onetp and 1 is key
-            data = new byte[2][];
-            for(int i = 0; i < 2; i++){data[i]=new byte[file.Length];} //Set the array to length of file
-            
-            Parallel.For(0, file.Length, ParallelEncryptMethod);
+            Parallel.For(0, data[0].Length, ParallelEncryptMethod);
 
             File.WriteAllBytes(pathName+".onetpm",data[0]);
             File.WriteAllBytes(pathName+".keym",data[1]);
         }
         static void ParallelEncryptMethod(int i)
         {
-            data[0][i] = (byte)(file[i]+randomBytes[i]);
-            data[1][i] = randomBytes[i];
+            data[0][i] = (byte)(data[0][i]+data[1][i]);
         }
     
         static void ParallelDecrypt(string encryptedText, string key)
         {
-            data = new byte[2][];
             data[0] = File.ReadAllBytes(encryptedText);
             data[1] = File.ReadAllBytes(key);
-            file = new byte[data[0].Length];
+
             string nameOfFile = Path.GetFileNameWithoutExtension(key);
 
-            Parallel.For(0, file.Length, ParallelDecryptMethod);
+            Parallel.For(0, data[0].Length, ParallelDecryptMethod);
 
             File.Delete(encryptedText);
             File.Delete(key);
-            File.WriteAllBytes(nameOfFile,file);
+            File.WriteAllBytes(nameOfFile,data[0]);
         }
 
         static void ParallelDecryptMethod(int i)
         {
-            file[i] = (byte)(data[0][i] - data[1][i]);
+            data[0][i] = (byte)(data[0][i] - data[1][i]);
         }
     }
 }
